699f0bb2b08a324d95f5d372d71e50874f7484b1
Andrade Sampaio <andrade.sampaio@outlook.com>
2024-05-22 18:37:17 -0300

    feat: update

--- internal/database/db.go
+++ internal/database/db.go
@@ -14,11 +14,11 @@ var (
 )
 
 func Connect() {
-	// os.Setenv("HOST", "localhost")
-	// os.Setenv("PORT", "5432")
-	// os.Setenv("USER", "root")
-	// os.Setenv("PASSWORD", "root")
-	// os.Setenv("DBNAME", "mydatabase")
+	os.Setenv("HOST", "localhost")
+	os.Setenv("PORT", "5432")
+	os.Setenv("USER", "root")
+	os.Setenv("PASSWORD", "root")
+	os.Setenv("DBNAME", "mydatabase")
 
 	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("PORT")+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
